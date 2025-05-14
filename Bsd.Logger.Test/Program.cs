3ead438159f8191e2a8719ae2fe1eb06578016c9
Willem Beltman <hwj.beltman@gmail.com>
2025-05-14 02:00:57 +0200

    Initial commit

---
+++ Bsd.Logger.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bsd.Logger;
+
+using (var logger = new ConsoleLogger())
+{
+    logger.StartThread();
+
+    logger.WriteLine("Hello, World! 1");
+    logger.ReWriteLine("Hello, World! 2");
+    logger.ReWriteLine("Hello, World! 3");
+    logger.ReWriteLine("Hello, World! 4"); 
+    logger.WriteLine("Hello, World! 5");
+}
